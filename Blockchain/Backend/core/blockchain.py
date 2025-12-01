2af95dc4d02f90e3bab191216e8d985cffba8f6c
Bernardo Perrot <bernardo.sp@aluno.ifsc.edu.br>
2025-12-01 16:59:38 -0300

    BlockChain data on Disk

--- Blockchain/Backend/core/blockchain.py
+++ Blockchain/Backend/core/blockchain.py
@@ -4,17 +4,25 @@ sys.path.append("/Users/Ana/Documents/GitHub/BlockChainPython")
 from Blockchain.Backend.core.block import Block
 from Blockchain.Backend.core.blockheader import BlockHeader
 from Blockchain.Backend.util.util import hash256
+from Blockchain.Backend.core.database.database import BlockchainDB
 import time
-import json
+
 
 ZERO_HASH = "0" *64
 VERSION = 1
 
 class Blockchain:
     def __init__(self):
-        self.chain = []
         self.GenensisBlock()
         
+    def write_on_disk(self, block):
+        blockchainDB = BlockchainDB()
+        blockchainDB.write(block)
+        
+    def fetch_last_block(self):
+        blockchainDB = BlockchainDB()
+        return blockchainDB.lastBlock()
+
     def GenensisBlock(self):
         BlockHeight = 0
         prevBlockHash  = ZERO_HASH
@@ -27,15 +35,14 @@ class Blockchain:
         bits = 'ffff001f'
         blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits)
         blockheader.mine()
-        self.chain.append(Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__)
-        print(self.chain)
-        print(json.dumps(self.chain, indent=4))
+        self.write_on_disk([Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__])
+
 
     def main(self):
         while True:
-            lastBlock = self.chain[::-1]
-            BlockHeight = lastBlock[0]["Height"] + 1
-            prevBlockHash = lastBlock[0]['BlockHeader']['blockHash']
+            lastBlock = self.fetch_last_block()
+            BlockHeight = lastBlock["Height"] + 1
+            prevBlockHash = lastBlock['BlockHeader']['blockHash']
             self.addBlock(BlockHeight, prevBlockHash)
         
         
